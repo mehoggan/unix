63494f7b8b86866c0906eb7e3d2a5bf070e3dec2
Matthew Hoggan <mehoggan@gmail.com>
2016-03-09 18:55:17 -0800

    Working on stuff.
    Change-Id: I7cef97ae9e0ae8b400dbc8055a70e7fcd285d7c0

--- sdl_tutorial/src/main.c
+++ sdl_tutorial/src/main.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 
-#include "SDL.h"
+//#include "SDL.h"
 #include "opengl_core.h"
 
 int main(int argc, char* argv[])
 {
   int status;
 
-  status = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
-  if (status < 0) {
-    exit(EXIT_FAILURE);
-  }
+//status = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+//if (status < 0) {
+//  exit(EXIT_FAILURE);
+//}
 
-  status = opengl_core_init(0);
+//status = opengl_core_init(0);
+  status = init(0);
 
   exit(0);
 }
